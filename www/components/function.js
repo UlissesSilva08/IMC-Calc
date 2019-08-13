be54f2d9fb6ea842735a6a586c1381b96f9673df
UlissesSilva08 <ulisses08@hotmail.com>
2019-08-13 14:04:08 +0900

    Finalização do App e primeiro teste no Build

--- www/components/function.js
+++ www/components/function.js
@@ -1,10 +1,11 @@
 function calcular(){
-  var nome, idade, peso, altura, sexo, imc;
+  var nome, idade, peso, altura, a, sexo, imc;
 
   nome = document.calc.nome.value;
   idade = parseFloat(document.calc.idade.value);
   peso = parseFloat(document.calc.peso.value);
   altura = parseFloat(document.calc.altura.value);
+  a = parseFloat(altura.toFixed(2));
   sexo = document.calc.sexo.value;
 
   if(idade <= 21)
@@ -20,6 +21,48 @@ function calcular(){
   else
   {
     document.calc.visor2.value = nome + ", Esta na Melhor idade!";
+  }
+
+  imc = peso / Math.pow(a, 2);
+
+  if(sexo == "mulher")
+  {
+    if(imc <= 18.4)
+    {
+      document.calc.visor1.value = "Seu IMC é: " + imc + ", Abaixo do peso!";
+    }
+    else if(imc <= 24)
+    {
+      document.calc.visor1.value = "Seu IMC é: " + imc + ", Peso Normal!";
+    }
+    else if(imc <= 29)
+    {
+      document.calc.visor1.value = "Seu IMC é: " + imc + ", Acima do Peso!";
+    }
+    else
+    {
+      document.calc.visor1.value = "Seu IMC é: " + imc + ", Obesidade!";
+    }
+  }
+
+  else
+  {
+    if(imc <= 19.4)
+    {
+      document.calc.visor1.value = "Seu IMC é: " + imc + ", Abaixo do peso!";
+    }
+    else if(imc <= 25)
+    {
+      document.calc.visor1.value = "Seu IMC é: " + imc + ", Peso Normal!";
+    }
+    else if(imc <= 30)
+    {
+      document.calc.visor1.value = "Seu IMC é: " + imc + ", Acima do peso!";
+    }
+    else
+    {
+      document.calc.visor1.value = "Seu IMC é: " + imc + ", Obesidade!";
+    }
   }
 }
 
