c6a91b4ef6cb9c861d7c45bea305a9b81179ea86
UlissesSilva08 <ulisses08@hotmail.com>
2019-08-14 03:39:40 +0900

    Finalização do App

--- www/components/function.js
+++ www/components/function.js
@@ -1,10 +1,11 @@
 function calcular(){
-  var nome, idade, peso, altura, a, sexo, tmbh, tmbm;
+  var nome, idade, peso, altura, a, sexo, nivel, tmbh, tmbm;
 
   nome = document.calc.nome.value;
   idade = parseFloat(document.calc.idade.value);
   peso = parseFloat(document.calc.peso.value);
   altura = parseFloat(document.calc.altura.value);
+  nivel = document.calc.nivel.value; 
   a = parseFloat(altura.toFixed(2));
   sexo = document.calc.sexo.value;
 
@@ -23,9 +24,9 @@ function calcular(){
     document.calc.visor2.value = nome + ", Esta na Melhor idade!";
   }
 
-  tmbh = (66.47 + (13.75 * peso) + ( 5.00 * a) - (6.76 * idade));
+  tmbh = nivel * (66 + (13.7 * peso) + ( 5.00 * a) - (6.8 * idade));
 
-  tmbm = (655.1 + (9.56 * peso) + ( 1.85 * a) - (4.68 * idade));
+  tmbm = nivel * (655 + (9.6 * peso) + ( 1.8 * a) - (4.7 * idade));
 
 
   if(sexo == "mulher")
